f407969a203d171b9ee01ccc68dd8f0d542312cc
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-05-02 12:32:06 -0500

    creando la base de datos

---
+++ Eventos/create_database.sql
@@ -0,0 +1,4 @@
+create user 'wilsondelgado'@'localhost' identified by "1102717619";
+create database fomulario_base_doom;
+grant all privileges on fomulario_base_doom.* To 'wilsondelgado'@'localhost';
+flush privileges;
